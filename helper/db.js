e3c3c5816d2c3d66d13810fb785668348ae7b707
Burak Tahtacıoğlu <clubwomanizer588@gmail.com>
2018-06-29 15:06:33 +0300

    mongo connect configuration changes

--- helper/db.js
+++ helper/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    mongoose.connect('mongodb://movie-user:q1w2e3r4t5y6u7@ds259499.mlab.com:59499/movie-api', {useMongoClient: true});
+    mongoose.connect('mongodb://movie-user:q1w2e3r4t5y6u7@ds259499.mlab.com:59499/movie-api');
     mongoose.connection.on('open', () =>{
-        console.log("MongoDB connected");
+        console.log("MongoDB Server Connected Successful(!)");
     });
     mongoose.connection.on('error', (err) =>{
-        console.log("MongoDB Error", err);
+        console.log("MongoDB Server Error(!)", err);
     });
 
     mongoose.Promise = global.Promise;
